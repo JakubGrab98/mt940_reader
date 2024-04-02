32653150efceeb6b891d2af7e0971c68794baf26
Grabarczyk <Jakub.Grabarczyk@colliers.com>
2024-04-02 15:06:49 +0200

    added parsing transaction in usd

--- fifo_calculation.py
+++ fifo_calculation.py
@@ -6,7 +6,7 @@ from fifo_excel_report import FifoExcelReport
 
 
 FILE_NAME = "transactions.json"
-EXPORT_PATH = r"C:\FIFO_APP\fifo-report.xlsx"
+EXPORT_PATH = r"C:\FIFO_APP\fifo-report-USD.xlsx"
 
 with open(FILE_NAME, "r") as jsonfile:
     transactions_list = json.load(jsonfile)
@@ -16,6 +16,17 @@ fifo_calculator.fifo_calculation()
 fifo_report = FifoExcelReport(fifo_calculator.fifo_logs)
 fifo_report.export_to_excel(EXPORT_PATH)
 
+
+# inflows = []
+# outflows = []
+
+# for transaction in transactions_list:
+#     if transaction["transaction_side"][0] == "C":# and transaction["transaction_date"][0:7] == "2016-12": #or transaction["transaction_date"][0:4] == "2016"):
+#         inflows.append(transaction["transaction_amount"])
+#     elif transaction["transaction_side"][0] == "D":# and transaction["transaction_date"][0:7] == "2016-12": # or transaction["transaction_date"][0:4] == "2016"):
+#         outflows.append(transaction["transaction_amount"])
+
+# print(sum(inflows) - sum(outflows))
 # transactions_list.sort(key=lambda x: x["id"])
 
 # fifo_queue = deque()
